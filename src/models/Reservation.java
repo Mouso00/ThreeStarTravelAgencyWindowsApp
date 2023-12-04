9315f7aa94396d404d3f3e4abe6fb48fd1fc8bf3
Mostafa Ahmed <m.ahmed@rdc.gr>
2023-12-04 17:43:56 +0200

    Fix some of the develop

--- src/models/Reservation.java
+++ src/models/Reservation.java
@@ -1,8 +1,8 @@
 package models;
 
 public class Reservation {
-	private int id;
-	private int trainNumber;
+    private int id;
+    private int trainNumber;
     private String from;
     private String to;
     private String date;
@@ -11,92 +11,86 @@ public class Reservation {
     private String seat;
     private double price;
     private String status;
-	private String closedByUserId;
+    private String userId;
 
     public Reservation() {
     }
 
-    public Reservation(int id,int trainNumber, String from, String to, String date, String time, String travelClass, String selectedSeat, double price, String userId) {
-        this.id =id;
+    public Reservation(int trainNumber, String from, String to, String date, String time, String travelClass, String seat, double price, String status, String userId) {
         this.trainNumber = trainNumber;
         this.from = from;
         this.to = to;
         this.date = date;
         this.time = time;
         this.travelClass = travelClass;
-        this.seat = selectedSeat;
+        this.seat = seat;
         this.price = price;
-    }
-    public int getId() {
-        return id;
+        this.status = status;
+        this.userId = userId;
     }
 
-    public void getId(int id) {
-        this.id = id;
-    }
- 
     public String getFrom() {
         return from;
     }
 
-    public void setFrom(String selectedFrom) {
-        this.from = selectedFrom;
+    public void setFrom(String from) {
+        this.from = from;
     }
 
     public String getTo() {
         return to;
     }
 
-    public void setTo(String selectedTo) {
-        this.to = selectedTo;
+    public void setTo(String to) {
+        this.to = to;
     }
 
     public String getDate() {
         return date;
     }
 
-    public void setDate(String selectedDate) {
-        this.date = selectedDate;
+    public void setDate(String date) {
+        this.date = date;
     }
-    
+
     public String getTime() {
         return time;
     }
 
-    public void setTime(String selectedTime) {
-        this.time = selectedTime;
+    public void setTime(String time) {
+        this.time = time;
     }
 
     public String getTravelClass() {
         return travelClass;
     }
 
-    public void setTravelClass(String selectedClass) {
-        this.travelClass = selectedClass;
+    public void setTravelClass(String travelClass) {
+        this.travelClass = travelClass;
     }
 
     public String getSeat() {
         return seat;
     }
 
-    public void setSeat(String selectedSeat) {
-        this.seat = selectedSeat;
+    public void setSeat(String seat) {
+        this.seat = seat;
     }
 
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double calculatePrice) {
-        this.price = calculatePrice;
+    public void setPrice(double price) {
+        this.price = price;
     }
 
     public String getStatus() {
         return status;
     }
 
-    public void setStatus(String string) {
-        this.status = string;
+    public void setStatus(String status) {
+        this.status = status;
     }
 
     public int getTrainNumber() {
@@ -107,5 +101,11 @@ public class Reservation {
         this.trainNumber = trainNumber;
     }
 
-  
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
--- src/services/ReservationDAO.java
+++ src/services/ReservationDAO.java
@@ -14,62 +14,95 @@ import java.util.Locale;
 
 import dbConnection.DatabaseConnection;
 import models.Reservation;
+import models.User;
 
 public class ReservationDAO {
-	private static final String GET_CITIES_QUERY = "SELECT * FROM cities";
+    private static final String GET_CITIES_QUERY = "SELECT * FROM cities";
+    private static final String INSERT_RESERVATION_QUERY = "INSERT INTO reservations (train_number, class_type, date_of_journey, source_location, destination_location, status, time_of_journey, seat, price, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-	public static String[] getCityOptions() {
-	    try (Connection connection = DatabaseConnection.getConnection();
-	         Statement statement = connection.createStatement();
-	         ResultSet resultSet = statement.executeQuery(GET_CITIES_QUERY)) {
+    public static String[] getCityOptions() {
+        try (Connection connection = DatabaseConnection.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(GET_CITIES_QUERY)) {
 
-	        List<String> cityOptions = new ArrayList<>();
-	        
-	        while (resultSet.next()) {
-	            String cityName = resultSet.getString("city_name");
-	            cityOptions.add(cityName);
-	        }
+            List<String> cityOptions = new ArrayList<>();
 
-	        return cityOptions.toArray(new String[0]);
+            while (resultSet.next()) {
+                String cityName = resultSet.getString("city_name");
+                cityOptions.add(cityName);
+            }
 
-	    } catch (SQLException e) {
-	        e.printStackTrace(); // Handle the exception according to your needs
-	    }
+            return cityOptions.toArray(new String[0]);
 
-	    return new String[]{""}; // Return a default value if there's an error
-	}
-	
-	private static final String INSERT_RESERVATION_QUERY = "INSERT INTO reservations (id, train_number, class_type, date_of_journey, source_location, destination_location, status,time_of_journey,seat,price) VALUES (?,?,?,?, ?, ?, ?, ?, ?,?)";
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception according to your needs
+        }
 
-	public static void insertReservation(Reservation reservation) {
-	    try (Connection connection = DatabaseConnection.getConnection();
-	         PreparedStatement preparedStatement = connection.prepareStatement(INSERT_RESERVATION_QUERY)) {
+        return new String[]{""}; // Return a default value if there's an error
+    }
 
-	    	 preparedStatement.setInt(1, reservation.getTrainNumber());
-	    	 preparedStatement.setInt(2, reservation.getTrainNumber());
-	            preparedStatement.setString(3, reservation.getTravelClass());
+    public static void insertReservation(Reservation reservation) {
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_RESERVATION_QUERY, Statement.RETURN_GENERATED_KEYS)) {
 
-	        // Convert the date string to the MySQL date format 'YYYY-MM-DD'
-	        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US);
-	        Date parsedDate = new Date(dateFormat.parse(reservation.getDate()).getTime());
-	        preparedStatement.setDate(4, parsedDate);
+            preparedStatement.setInt(1, reservation.getTrainNumber());
+            preparedStatement.setString(2, reservation.getTravelClass());
 
-	        preparedStatement.setString(5, reservation.getFrom());
-	        preparedStatement.setString(6, reservation.getTo());
-	        preparedStatement.setString(7, reservation.getStatus());
-	        preparedStatement.setString(8, reservation.getTime());
-	        preparedStatement.setString(9, reservation.getSeat());
-	        preparedStatement.setDouble(10, reservation.getPrice());
+            // Convert the date string to the MySQL date format 'YYYY-MM-DD'
+            SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy", Locale.US);
+            Date parsedDate = new Date(dateFormat.parse(reservation.getDate()).getTime());
+            preparedStatement.setDate(3, parsedDate);
 
+            preparedStatement.setString(4, reservation.getFrom());
+            preparedStatement.setString(5, reservation.getTo());
+            preparedStatement.setString(6, reservation.getStatus());
+            preparedStatement.setString(7, reservation.getTime());
+            preparedStatement.setString(8, reservation.getSeat());
+            preparedStatement.setDouble(9, reservation.getPrice());
+            preparedStatement.setString(10, reservation.getUserId());
 
-	        preparedStatement.executeUpdate();
+            preparedStatement.executeUpdate();
 
-	    } catch (SQLException | ParseException e) {
-	        e.printStackTrace(); // Handle the exception according to your needs
-	    }
-	}
+        } catch (SQLException | ParseException e) {
+            e.printStackTrace(); // Handle the exception according to your needs
+        }
+    }
 
-	
+    // Assuming that the User table has a column named 'id' for the user's ID
+    private static final String INSERT_USER_QUERY = "INSERT INTO users (username, password, role, full_name, email_address, gender, date_of_birth) VALUES (?, ?, ?, ?, ?, ?, ?)";
 
+    public static int insertUser(User user) {
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USER_QUERY, Statement.RETURN_GENERATED_KEYS)) {
 
+            preparedStatement.setString(1, user.getUsername());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getRole());
+            preparedStatement.setString(4, user.getFull_name());
+            preparedStatement.setString(5, user.getEmail_address());
+            preparedStatement.setString(6, user.getGender());
+            preparedStatement.setDate(7, user.getDate_of_birth());
+
+            int affectedRows = preparedStatement.executeUpdate();
+            if (affectedRows > 0) {
+                ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    return generatedKeys.getInt(1);
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace(); // Handle the exception according to your needs
+        }
+
+        return -1; // Return -1 for failure
+    }
+
+    public static void insertReservationWithUser(Reservation reservation, User user) {
+        int userId = insertUser(user);
+        if (userId != -1) {
+            reservation.setUserId(String.valueOf(userId));
+            insertReservation(reservation);
+        }
+    }
 }
